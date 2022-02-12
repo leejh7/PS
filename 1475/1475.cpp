b5e0fcd32a48f58ca74093177d92f794a00809ce
leejh7 <777joonho@kookmin.ac.kr>
2022-02-12 23:47:35 +0900

    [Easy] Implementation

--- 1475/1475.cpp
+++ 1475/1475.cpp
@@ -9,19 +9,22 @@ int main()
     cin.tie(NULL);
     freopen("/Volumes/Programming/C:C++/input.txt", "r", stdin);
 
-    int arr[10] = {0,};
+    int arr[10] = {
+        0,
+    };
 
     string input;
     cin >> input;
 
-    for(int i=0; i<input.length(); i++) {
+    for (int i = 0; i < input.length(); i++)
+    {
         arr[input[i] - '0']++;
     }
 
     arr[9] = (arr[9] + arr[6] + 1) / 2;
     arr[6] = arr[9];
 
-    cout << *max_element(arr, arr+10);
+    cout << *max_element(arr, arr + 10);
 
     return 0;
 }
