7e7db4c580a9fb1fd26d686a7e0c99a70da666f6
leejh7 <777joonho@kookmin.ac.kr>
2022-02-12 15:59:58 +0900

    [Easy] Implementation

---
+++ 1475/1475.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    freopen("/Volumes/Programming/C:C++/input.txt", "r", stdin);
+
+    int arr[10] = {0,};
+
+    string input;
+    cin >> input;
+
+    for(int i=0; i<input.length(); i++) {
+        arr[input[i] - '0']++;
+    }
+
+    arr[9] = (arr[9] + arr[6] + 1) / 2;
+    arr[6] = arr[9];
+
+    cout << *max_element(arr, arr+10);
+
+    return 0;
+}
